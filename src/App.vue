97bb28642ed08b197cd2b1af496e3e0b984da7f2
Naya <naya252naya@gmail.com>
2023-06-16 23:03:30 +0300

    feat: add header component

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,7 @@
 <template>
   <v-app v-resize="onResize">
-    <v-app-bar
-      fixed
-      dark
-      shrink-on-scroll
-      prominent
-      :src="require('./assets/Baner2.png')"
-      fade-img-on-scroll
-      class="menu"
-      height="250px"
-      style="z-index: 150"
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to bottom right, rgba(0,0,0,.4), rgba(0,0,0,.95)"
-        >
-        </v-img>
-      </template>
-
-      <v-app-bar-title
-        class="ml-3"
-        style="width: auto; font-weight: 600"
-        :style="windowSize.x < 600 ? 'height:60px' : ''"
-      >
-        <img
-          height="50"
-          :src="require('./assets/Purple.svg')"
-          alt="Crypto_Deer"
-        />
-      </v-app-bar-title>
-      <v-app-bar-title
-        v-if="windowSize.x > 600"
-        class="ml-3 mb-3"
-        style="width: auto; font-weight: 600"
-      >
-        Crypto_Deers
-      </v-app-bar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-toolbar-items style="height: 56px; max-height: 56px">
-        <v-tabs
-          :value="tab"
-          right
-          color="#fff"
-          height="56px"
-          background-color="transparent"
-        >
-          <v-tab @click="scrollToSection('section1')"
-            ><v-icon>mdi-home-flood</v-icon>
-            <span class="ml-3" v-if="windowSize.x > 800">Home</span>
-          </v-tab>
-          <v-tab @click="scrollToSection('section2')">
-            <v-icon>mdi-lightbulb-variant</v-icon>
-            <span class="ml-3" v-if="windowSize.x > 800">About</span>
-          </v-tab>
-          <v-tab @click="scrollToSection('section3')">
-            <v-icon>mdi-script-text-outline</v-icon>
-            <span class="ml-3" v-if="windowSize.x > 800">Rarity</span>
-          </v-tab>
-          <v-tab @click="scrollToSection('section4')">
-            <v-icon>mdi-account-group-outline</v-icon>
-            <span class="ml-3" v-if="windowSize.x > 800">Team</span>
-          </v-tab>
-        </v-tabs>
-      </v-toolbar-items>
-    </v-app-bar>
-    <v-main style="background: #1E1E1E">
+    <HeaderComponent :tab="tab" @changeDisabled="disabledCalculateTab = true" />
+    <v-main style="background: #1e1e1e">
       <v-container
         style="height: 100%"
         fluid
@@ -100,7 +34,7 @@
           "
         >
           <component :is="`Section${num}`" />
-          <Footer v-if="num === 4" />
+          <FooterComponent v-if="num === 4" />
         </section>
       </v-container>
     </v-main>
@@ -113,7 +47,8 @@ import Section1 from './components/Section1';
 import Section2 from './components/Section2';
 import Section3 from './components/Section3';
 import Section4 from './components/Section4';
-import Footer from './components/FooterComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
 
 import Twitter from './components/social/Twitter.vue';
 import OpenSea from './components/social/OpenSea.vue';
@@ -123,7 +58,7 @@ import { META_DATA } from './plugins/meta';
 export default {
   name: 'App',
   metaInfo() {
-    return META_DATA
+    return META_DATA;
   },
 
   components: {
@@ -131,7 +66,8 @@ export default {
     Section2,
     Section3,
     Section4,
-    Footer,
+    FooterComponent,
+    HeaderComponent,
     OpenSea,
     Twitter,
   },
@@ -144,16 +80,13 @@ export default {
     tab: 0,
     arr: [],
     disabledCalculateTab: false,
-
-    offsetTop: 0,
   }),
-  created() {},
   mounted() {
     this.onResize();
   },
-
   computed: {
     ...mapGetters(['GET_ACTIVE_ANIMATE']),
+
     calculateStyle() {
       return this.windowSize.x > 1020 && this.windowSize.y > 750
         ? 'min-height: 100dvh; display: flex; align-items: center;'
@@ -163,14 +96,6 @@ export default {
   methods: {
     ...mapMutations(['TOGGLE_ACTIVE_ANIMATE', 'SET_WINDOW_SIZE']),
 
-    scrollToSection(name) {
-      this.disabledCalculateTab = true;
-
-      document.getElementById(name).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    },
     notDisabled() {
       this.disabledCalculateTab = false;
     },
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-app-bar
+    fixed
+    dark
+    shrink-on-scroll
+    prominent
+    :src="require('../assets/Baner2.png')"
+    fade-img-on-scroll
+    class="menu"
+    height="250px"
+    style="z-index: 150"
+  >
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to bottom right, rgba(0,0,0,.4), rgba(0,0,0,.95)"
+      >
+      </v-img>
+    </template>
+
+    <v-app-bar-title
+      class="ml-3"
+      style="width: auto; font-weight: 600"
+      :style="GET_WINDOW_SIZE.x < 600 ? 'height:60px' : ''"
+    >
+      <img
+        height="50"
+        :src="require('../assets/Purple.svg')"
+        alt="Crypto_Deer"
+      />
+    </v-app-bar-title>
+    <v-app-bar-title
+      v-if="GET_WINDOW_SIZE.x > 600"
+      class="ml-3 mb-3"
+      style="width: auto; font-weight: 600"
+    >
+      Crypto_Deers
+    </v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items style="height: 56px; max-height: 56px">
+      <v-tabs
+        :value="tab"
+        right
+        color="#fff"
+        height="56px"
+        background-color="transparent"
+      >
+        <v-tab
+          v-for="item in tabs"
+          :key="item.id"
+          @click="scrollToSection(item.id)"
+          ><v-icon>{{ item.icon }}</v-icon>
+          <span class="ml-3" v-if="GET_WINDOW_SIZE.x > 800">{{
+            item.name
+          }}</span>
+        </v-tab>
+      </v-tabs>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'HeaderComponent',
+
+  props: {
+    tab: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      tabs: [
+        { id: 'section1', icon: 'mdi-home-flood', name: 'Home' },
+        { id: 'section2', icon: 'mdi-lightbulb-variant', name: 'About' },
+        { id: 'section3', icon: 'mdi-script-text-outline', name: 'Rarity' },
+        { id: 'section4', icon: 'mdi-account-group-outline', name: 'Team' },
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters(['GET_WINDOW_SIZE']),
+  },
+  methods: {
+    scrollToSection(name) {
+      this.$emit('changeDisabled');
+
+      document.getElementById(name).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    },
+  },
+};
+</script>
