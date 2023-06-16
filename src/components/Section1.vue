da29f16faeeee04aed5f513e3aa9d34d98a629e8
Naya <naya252naya@gmail.com>
2023-06-17 02:02:41 +0300

    fix: section 1

--- src/components/Section1.vue
+++ src/components/Section1.vue
@@ -1,193 +1,198 @@
 <template>
-  <v-container>
-    <v-row class="text-center ma-0" itemscope itemtype="https://schema.org/CreativeWork">
-      <v-col
-        order="1"
-        order-sm="0"
-        class="mb-5 pl-md-6 col-12 col-sm-6"
-        style="display: flex; justify-content: flex-end"
+  <v-row
+    class="text-center ma-0"
+    style="padding-top: 12rem"
+    itemscope
+    itemtype="https://schema.org/CreativeWork"
+  >
+    <v-col
+      min-height="100%"
+      order="1"
+      order-sm="0"
+      class="mb-5 pl-md-6 col-12 col-sm-6"
+      style="display: flex; justify-content: flex-end"
+    >
+      <v-card
+        flat
+        :loading="loading"
+        class="mr-md-12 rotate"
+        style="border-radius: 10%; position: relative"
+        min-width="250px"
+        max-width="650px"
+        min-height="calc(100dvh - 12rem)"
       >
-        <v-card
-          flat
-          :loading="loading"
-          class="mr-md-12 rotate"
-          style="border-radius: 10%; position: relative"
-          min-width="250px"
-          max-width="650px"
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <a
+          href="https://opensea.io/collection/crypto-deers"
+          target="_blank"
+          class="msg"
+          style="
+            position: absolute;
+            top: 50%;
+            right: 0;
+            z-index: 6;
+            color: #fff;
+            border: 2px dashed #9466ff;
+            border-radius: 50%;
+          "
+          :style="GET_WINDOW_SIZE.x < 1260 ? 'font-size: 0.85rem' : '1rem'"
+          :class="GET_WINDOW_SIZE.x < 1260 ? 'px-3 py-6' : 'pa-10'"
         >
-          <template slot="progress">
-            <v-progress-linear
-              color="deep-purple"
-              height="10"
-              indeterminate
-            ></v-progress-linear>
-          </template>
+          Купи Олешку
+        </a>
 
-          <a
-            href="https://opensea.io/collection/crypto-deers"
-            target="_blank"
-            class="msg"
-            style="
-              position: absolute;
-              top: 50%;
-              right: 0;
-              z-index: 6;
-              color: #fff;
-              border: 2px dashed #9466ff;
-              border-radius: 50%;
-            "
-            :style="GET_WINDOW_SIZE.x < 1260 ? 'font-size: 0.85rem' : '1rem'"
-            :class="GET_WINDOW_SIZE.x < 1260 ? 'px-3 py-6' : 'pa-10'"
-          >
-            Купи Олешку
-          </a>
-
-          <v-img itemprop="image" min-height="250" :src="require(`../assets/first/${gl}`)">
+        <v-img
+          itemprop="image"
+          min-height="250"
+          :src="require(`../assets/first/${activeGlitch}`)"
+        >
+          <v-img
+            style="position: absolute; top: 0; left: 0; z-index: 1"
+            min-height="250"
+            :src="require(`../assets/first/deer1.svg`)"
+          ></v-img>
+          <template v-for="(deer, i) in deers">
             <v-img
-              style="position: absolute; top: 0; left: 0; z-index: 1"
-              min-height="250"
-              :src="require(`../assets/first/deer1.svg`)"
-            ></v-img>
-            <v-img
+              :key="i"
+              v-show="active === i"
               style="position: relative; z-index: 5"
               min-height="250"
-              :src="require(`../assets/first/${img}.svg`)"
+              :src="require(`../assets/first/${deer}.svg`)"
             ></v-img>
-          </v-img>
-        </v-card>
-      </v-col>
+          </template>
+        </v-img>
+      </v-card>
+    </v-col>
 
-      <v-col
-        order="0"
-        order-sm="1"
-        class="mb-5 pt-12 col-12 col-sm-6 px-0"
-        style="display: flex; justify-content: center; flex-direction: column"
+    <v-col
+      min-height="100%"
+      order="0"
+      order-sm="1"
+      class="mb-5 pt-12 col-12 col-sm-6 px-0"
+      style="display: flex; justify-content: center; flex-direction: column"
+    >
+      <div style="max-width: 650px">
+        <h1 class="display-2 mb-10" style="font-weight: 600" itemprop="name">
+          Welcome to Crypto_Deers
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Crypto_Deers is a collection of 10 000 unique deers
+          <br v-if="GET_WINDOW_SIZE.x > 800" />who live on the metaverse
+        </p>
+      </div>
+      <div
+        class="mt-10"
+        style="
+          max-width: 650px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        "
       >
-        <div style="max-width: 650px">
-          <h1 class="display-2 mb-10" style="font-weight: 600" itemprop="name">
-            Welcome to Crypto_Deers
-          </h1>
-
-          <p class="subheading font-weight-regular">
-            Crypto_Deers is a collection of 10 000 unique deers
-            <br v-if="GET_WINDOW_SIZE.x > 800" />who live on the metaverse
-          </p>
-        </div>
-        <div
-          class="mt-10"
-          style="
-            max-width: 650px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-          "
-        >
-          <OpenSea />
-          <Twitter />
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+        <OpenSea />
+        <Twitter />
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from "vuex";
-import Twitter from "../components/social/Twitter.vue";
-import OpenSea from "../components/social/OpenSea.vue";
+import { mapGetters } from 'vuex';
+import Twitter from '../components/social/Twitter.vue';
+import OpenSea from '../components/social/OpenSea.vue';
 export default {
-  name: "Section1",
+  name: 'Section1',
   components: {
     OpenSea,
     Twitter,
   },
   props: {
     tab: {
-      type: Boolean,
+      type: Number,
       require: true,
-      default: false,
     },
   },
 
   data: () => ({
-    img: "deer",
-    gl: "notGl.png",
-    glithes: ["gl3", "gl2", "gl", "gl1"],
+    img: 'deer',
+    gl: 'notGl.png',
+    glitches: ['gl3', 'gl2', 'gl', 'gl1'],
     deers: [
-      "deer",
-      "deer1",
-      "deer2",
-      "deer3",
-      "deer4",
-      "deer5",
-      "deer6",
-      "deer7",
-      "deer8",
-      "deer9",
-      "deer10",
-      "deer",
-      "deer1",
-      "deer2",
-      "deer3",
-      "deer4",
-      "deer5",
-      "deer6",
-      "deer7",
-      "deer8",
-      "deer9",
-      "deer10",
+      'deer',
+      'deer1',
+      'deer2',
+      'deer3',
+      'deer4',
+      'deer5',
+      'deer6',
+      'deer7',
+      'deer8',
+      'deer9',
+      'deer10',
+      'deer',
+      'deer1',
+      'deer2',
+      'deer3',
+      'deer4',
+      'deer5',
+      'deer6',
+      'deer7',
+      'deer8',
+      'deer9',
+      'deer10',
     ],
-    count: 0,
     images: 22,
 
     loading: false,
   }),
 
   mounted() {
-    if (this.tab) {
-      this.changeDeer();
-    }
+    this.changeActive();
   },
   computed: {
-    ...mapGetters(["GET_ACTIVE_ANIMATE", "GET_WINDOW_SIZE"]),
+    ...mapGetters(['GET_WINDOW_SIZE']),
+    active() {
+      console.log(this.images - 1);
+      return this.images - 1;
+    },
+    activeGlitch() {
+      let val;
+      if (this.active === 21) {
+        val = `${this.glitches[1]}.svg`;
+      } else if (this.active === 15) {
+        val = `${this.glitches[0]}.svg`;
+      } else if (this.active === 10) {
+        val = `${this.glitches[2]}.svg`;
+      } else if (this.active === 4) {
+        val = `${this.glitches[3]}.svg`;
+      } else {
+        val = this.gl;
+      }
+      return val;
+    },
   },
   methods: {
-    ...mapMutations(["TOGGLE_ACTIVE_ANIMATE"]),
-    changeDeer() {
-      this.count++;
-      this.images--;
-      this.img = this.deers[this.images];
-      //   console.log(this.img);
-      this.gl = "notGl.png";
-      if (this.images == 0) {
-        this.images = 22;
-        this.gl = "gl2.svg";
-      }
-      if (this.images == 16) {
-        this.gl = "gl3.svg";
-      }
-      if (this.images == 11) {
-        this.gl = "gl.svg";
-      }
-      if (this.images == 5) {
-        this.gl = "gl1.svg";
-      }
-
-      if (this.tab) {
-        setTimeout(this.changeDeer, 200);
-      } else {
-        this.count = 0;
-        this.images = 22;
-        this.gl = "notGl.png";
-        this.TOGGLE_ACTIVE_ANIMATE(false);
+    changeActive() {
+      if (this.tab === 0) {
+        this.images--;
+        if (this.images == 0) {
+          this.images = 22;
+        }
+        setTimeout(this.changeActive, 200);
       }
     },
   },
   watch: {
     tab() {
-      if (this.tab && !this.GET_ACTIVE_ANIMATE) {
-        this.TOGGLE_ACTIVE_ANIMATE(true);
-        this.changeDeer();
-      }
+      this.changeActive();
     },
   },
 };
@@ -215,4 +220,3 @@ export default {
   }
 }
 </style>
-
